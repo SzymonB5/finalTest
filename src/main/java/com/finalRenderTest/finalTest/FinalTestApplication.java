a9b05ea6a64cac19f4d993edcf0d4c5a19a6a555
SzymonB5 <235831@edu.p.lodz.pl>
2023-06-15 12:58:37 +0200

    Initial commit

---
+++ src/main/java/com/finalRenderTest/finalTest/FinalTestApplication.java
@@ -0,0 +1,22 @@
+package com.finalRenderTest.finalTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("")
+public class FinalTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalTestApplication.class, args);
+	}
+
+	@GetMapping
+	public String test() {
+		return "Works, Ok";
+	}
+}
